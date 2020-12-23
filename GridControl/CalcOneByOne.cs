2220dde335f871a8070a28115ba68a4832857de8
wly86 <451215954@qq.com>
2020-12-23 14:19:02 +0800

    按省份计算兼容

--- GridControl/CalcOneByOne.cs
+++ GridControl/CalcOneByOne.cs
@@ -15,10 +15,10 @@ namespace GridControl
         // 每个省对应一个数据库连接，每个连接里包含了降雨切片目录
         public static Dictionary<string, Dictionary<string, string>> dbValues = ClientConn.m_dbTableTypes;
         public static Dictionary<string, Dictionary<string, DataTable>> dbTableConfigs = ClientConn.m_dbTableConfig;
-        //！ pid列表，通过定时查询用来判断是否清空
-        public static Dictionary<int, int> pids = new Dictionary<int, int>();
-        
-        public static bool StartOneByOneExecsingle(string appPath, string computerunit)
+        //！ pid列表，通过定时查询用来判断是否清空key 是pid value是单元信息
+        public static Dictionary<int, string> pids = new Dictionary<int, string>();
+
+        public static bool StartOneByOneExecsingle(string appPath, string appunitInfo)
         {
             if (!File.Exists(appPath))
             {
@@ -43,7 +43,7 @@ namespace GridControl
 
                 if (isStart)
                 {
-                    pids.Add(int.Parse(computerunit), myProcess.Id);
+                    pids.Add(myProcess.Id, appunitInfo);
                 }
                 else
                 {
@@ -64,14 +64,15 @@ namespace GridControl
 
             return true;
         }
-        
+
         public static bool runBySingleCC()
         {
             // 更新execpath的值
-            string start = "2003-11-15T13:00";
-            string end = "2003-11-15T13:00";
+            string start = "2000-01-01T00:00";
+            string end = "2000-01-01T08:00";
             string datnums = "95";
-            string yearmmddForID = "2013111513";
+
+            string yearmmddForID = "2000010100";
             //! 遍历指定目录下的降雨数据
             if (!Directory.Exists(HookHelper.rainSRCDirectory))
             {
@@ -92,41 +93,6 @@ namespace GridControl
                 {
                     //! key名称，省份的名称
                     string keyString = curDB.Key;
-
-                    //！判断记录省份apppath的 pid列表是否为null，则说明省份执行结束了，否则进入等待
-                    //1\查询pids是否为空
-                    while (pids.Count > 0)
-                    {
-                        //! 执行等待，然后查询更新pids列表.等待5分钟
-                        Console.WriteLine(string.Format("等待前一个省份区域进程计算完成并关闭，pid进程查询更新等待中...") + DateTime.Now);
-                        System.Threading.Thread.Sleep(1000 * 60 * 1);
-
-                        //！ 遍历pids，查询windows process中是否存在这个pid，不存在，则移除
-                        int pidnum = pids.Count;
-                        foreach (var item in pids.ToList())
-                        {
-                            int curPID = item.Value;
-                            Process curProcss = null;
-                            try
-                            {
-                                curProcss = Process.GetProcessById(curPID);
-                            }
-                            catch (Exception ex)
-                            {
-                                curProcss = null;
-                            }
-                            bool isInProcess = curProcss == null ? false : true;
-                            if (!isInProcess)
-                            {
-                                pids.Remove(item.Key);
-                            }
-                        }
-
-                        if (pids.Count == 0)
-                        {
-                            break;
-                        }
-                    }
 
                     //！ 每个省的切片目录,对每个省执行两步操作
                     //! 1、根据指定的src降雨目录，执行python切片，输出路径为tile指定的目录
@@ -207,34 +173,192 @@ namespace GridControl
                             {
                                 //！每个省下的所有app
                                 int appnum = dbTableConfigs[keyString]["HSFX_ComputeUnit"].Rows.Count;
-                                for (int a = 0; a < appnum; ++a)
+                                //!判断appnum 是否超过了processnum，是则部分启动，等待
+                                //!先根据个数分组，分组后，则循环，则可以等待了
+                                int processGroup = (int)Math.Ceiling((float)appnum / (float)HookHelper.processnum);
+
+
+                                for (int g = 0; g < processGroup; ++g)
                                 {
-                                    //!当前路径
-                                    string apppath = dbTableConfigs[keyString]["HSFX_ComputeUnit"].Rows[a]["AppPath"].ToString();
-                                    string ComputeUnit = dbTableConfigs[keyString]["HSFX_ComputeUnit"].Rows[a]["ComputeUnit"].ToString();
-                                    //execbat路径
-                                    string execpath = apppath + "execsingle.bat";
-                                    if (apppath.EndsWith("/"))
+                                    //!循环每个组，pid存在，则执行等待
+                                    int perGroupCount = 0;
+                                    while (pids.Count > 0)
                                     {
-                                        execpath = apppath + "execsingle.bat";
-                                    }
-                                    else
-                                    {
-                                        execpath = apppath + "\\" + "execsingle.bat";
+                                        //! 执行等待，然后查询更新pids列表.等待1分钟
+                                        Console.WriteLine(string.Format("共{0}个分组", processGroup) + DateTime.Now);
+                                        Console.WriteLine(string.Format("等待第{0}场{1}文件的第{2}进程组计算完成并关闭，pid进程查询更新等待中，等待时长15秒...", d + 1, curDatFullname, g) + DateTime.Now);
+                                        System.Threading.Thread.Sleep(1000 * 15 * 1);
+
+                                        //kill
+                                        perGroupCount++;
+                                        Console.WriteLine(string.Format("已经等待次数{0}次", perGroupCount) + DateTime.Now);
+                                        if (perGroupCount >= 60)
+                                        {
+                                            //遍历强制关闭当前场次的所有pid程序
+                                            foreach (var item in pids.ToList())
+                                            {
+                                                int curPID = item.Key;
+                                                Process curProcss = null;
+                                                try
+                                                {
+                                                    curProcss = Process.GetProcessById(curPID);
+                                                }
+                                                catch (Exception ex)
+                                                {
+                                                    curProcss = null;
+                                                }
+                                                bool isInProcess = curProcss == null ? false : true;
+                                                if (isInProcess)
+                                                {
+                                                    curProcss.Kill();
+                                                }
+                                            }
+                                        }
+
+                                        //！ 遍历pids，查询windows process中是否存在这个pid，不存在，则移除
+                                        int pidnum = pids.Count;
+                                        foreach (var item in pids.ToList())
+                                        {
+                                            int curPID = item.Key;
+                                            Process curProcss = null;
+                                            try
+                                            {
+                                                curProcss = Process.GetProcessById(curPID);
+                                            }
+                                            catch (Exception ex)
+                                            {
+                                                curProcss = null;
+                                            }
+                                            bool isInProcess = curProcss == null ? false : true;
+                                            if (!isInProcess)
+                                            {
+                                                pids.Remove(item.Key);
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine(string.Format("单元{0}所在分组{1}计算进行中......需继续等待......", item.Value, g) + DateTime.Now);
+                                            }
+                                        }
+
+                                        if (pids.Count == 0)
+                                        {
+                                            break;
+                                        }
                                     }
 
-
-                                    //! 启动该exec.bat
-                                    bool isOneStart = StartOneByOneExecsingle(execpath, ComputeUnit);
-                                    if (isOneStart)
+                                    //当前分组的起始值，和end值
+                                    int startPROCESS = g * HookHelper.processnum;
+                                    int endPROCESS = (g + 1) * HookHelper.processnum;
+                                    if (g == processGroup - 1)
                                     {
-                                        Console.WriteLine(string.Format("{0}区域{1}执行成功  ", keyString, execpath) + DateTime.Now);
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine(string.Format("{0}区域{1}执行失败  ", keyString, execpath) + DateTime.Now);
+                                        endPROCESS = appnum;
                                     }
 
+                                    int validStartUnitModel = 0;
+                                    for (int a = startPROCESS; a < endPROCESS; ++a)
+                                    {
+                                        //!当前路径
+                                        string apppath = dbTableConfigs[keyString]["HSFX_ComputeUnit"].Rows[a]["AppPath"].ToString();
+                                        string ComputeUnit = dbTableConfigs[keyString]["HSFX_ComputeUnit"].Rows[a]["ComputeUnit"].ToString();
+                                        string ComputeNode = dbTableConfigs["china"]["HSFX_ComputeUnit"].Rows[a]["ComputeNode"].ToString();
+                                        //execbat路径
+                                        string execpath = apppath + "execsingle.bat";
+                                        if (apppath.EndsWith("/"))
+                                        {
+                                            execpath = apppath + "execsingle.bat";
+                                        }
+                                        else
+                                        {
+                                            execpath = apppath + "\\" + "execsingle.bat";
+                                        }
+
+
+                                        //! 启动该exec.bat
+                                        //! 单元信息
+                                        string appunitInfo = ComputeNode + "_" + ComputeUnit + "_" + apppath;
+                                        bool isOneStart = StartOneByOneExecsingle(execpath, appunitInfo);
+                                        if (isOneStart)
+                                        {
+                                            validStartUnitModel++;
+                                            //Console.WriteLine(string.Format("{0}节点{1}单元{2}路径执行成功  ", HookHelper.computerNode, ComputeUnit, execpath) + DateTime.Now);
+                                        }
+                                        else
+                                        {
+                                            //Console.WriteLine(string.Format("{0}节点{1}单元{2}路径执行失败  ", HookHelper.computerNode, ComputeUnit, execpath) + DateTime.Now);
+                                        }
+
+                                    }
+                                    Console.WriteLine(string.Format("{0}节点{1}个有效单元启动命令执行成功  ", HookHelper.computerNode, validStartUnitModel) + DateTime.Now);
+                                }
+
+                                //!循环每个组，pid存在，则执行等待，直至继续运行到下一步，代表一个场次计算结束
+                                int perWaitCount = 0;  //如果等待超过1个小时，仍然无法计算，则跳过这个场次，并写出到log中
+                                while (pids.Count > 0)
+                                {
+                                    //! 执行等待，然后查询更新pids列表.等待1分钟
+                                    Console.WriteLine(string.Format("等待第{0}场{1}文件计算完成并关闭，pid进程查询更新等待中，等待时长15秒...", d + 1, curDatFullname) + DateTime.Now);
+                                    System.Threading.Thread.Sleep(1000 * 15 * 1);
+                                    perWaitCount++;
+                                    Console.WriteLine(string.Format("已经等待次数{0}次", perWaitCount) + DateTime.Now);
+                                    if (perWaitCount >= 60)
+                                    {
+                                        //遍历强制关闭当前场次的所有pid程序
+                                        //将该场次值写出到log文件中
+                                        string ignoreCCName = curDatFullname;
+                                        WriteLog.AppendLogMethod(ignoreCCName, "datIgnore");
+                                        foreach (var item in pids.ToList())
+                                        {
+                                            int curPID = item.Key;
+                                            Process curProcss = null;
+                                            try
+                                            {
+                                                curProcss = Process.GetProcessById(curPID);
+                                            }
+                                            catch (Exception ex)
+                                            {
+                                                curProcss = null;
+                                            }
+                                            bool isInProcess = curProcss == null ? false : true;
+                                            if (isInProcess)
+                                            {
+                                                curProcss.Kill();
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine(string.Format("最后一组单元{0}计算进行中......需继续等待......", item.Value) + DateTime.Now);
+                                            }
+                                        }
+                                    }
+
+                                    //！ 遍历pids，查询windows process中是否存在这个pid，不存在，则移除
+                                    int pidnum = pids.Count;
+                                    foreach (var item in pids.ToList())
+                                    {
+                                        int curPID = item.Key;
+                                        Process curProcss = null;
+                                        try
+                                        {
+                                            curProcss = Process.GetProcessById(curPID);
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            curProcss = null;
+                                        }
+                                        bool isInProcess = curProcss == null ? false : true;
+                                        if (!isInProcess)
+                                        {
+                                            pids.Remove(item.Key);
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine(string.Format("最后一组单元{0}计算进行中......需继续等待......", item.Value) + DateTime.Now);
+                                        }
+                                    }
+
+                                    if (pids.Count == 0)
+                                    {
+                                        break;
+                                    }
                                 }
                             }
                             else
