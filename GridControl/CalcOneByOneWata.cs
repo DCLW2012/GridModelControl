a3e02cb810bab4f3a6926383aa4eb681b5d25fcb
wly86 <451215954@qq.com>
2021-02-22 15:17:52 +0800

    等待时间5秒

--- GridControl/CalcOneByOneWata.cs
+++ GridControl/CalcOneByOneWata.cs
@@ -243,8 +243,8 @@ namespace GridControl
                 while (pids.Count > 0)
                 {
                     //! 执行等待，然后查询更新pids列表.等待1分钟
-                    Console.WriteLine(string.Format("等待第{0}场{1}文件计算完成并关闭，pid进程查询更新等待中，等待时长15秒...",d+1, curDatFullname) + DateTime.Now);
-                    System.Threading.Thread.Sleep(1000 * 15 * 1);
+                    Console.WriteLine(string.Format("等待第{0}场{1}文件计算完成并关闭，pid进程查询更新等待中，等待时长5秒...",d+1, curDatFullname) + DateTime.Now);
+                    System.Threading.Thread.Sleep(1000 * 5 * 1);
                     perWaitCount++;
                     Console.WriteLine(string.Format("已经等待次数{0}次", perWaitCount) + DateTime.Now);
                     if (perWaitCount >= 60)
