b2ccafefb701e8dadba76af39b30277285e98619
longyangzz <longyangzz@outlook.com>
2025-06-13 21:25:05 +0800

    fix: 等待5秒

--- GridControl/CalcOneByOneWata.cs
+++ GridControl/CalcOneByOneWata.cs
@@ -206,8 +206,8 @@ namespace GridControl
                         {
                             //! 执行等待，然后查询更新pids列表.等待1分钟
                             Console.WriteLine(string.Format("共{0}个分组", processGroup) + DateTime.Now);
-                            Console.WriteLine(string.Format("等待第{0}场{1}文件的第{2}进程组计算完成并关闭，pid进程查询更新等待中，等待时长15秒...", d + 1, curDatFullname, g) + DateTime.Now);
-                            System.Threading.Thread.Sleep(1000 * 15 * 1);
+                            Console.WriteLine(string.Format("等待第{0}场{1}文件的第{2}进程组计算完成并关闭，pid进程查询更新等待中，等待时长5秒...", d + 1, curDatFullname, g) + DateTime.Now);
+                            System.Threading.Thread.Sleep(1000 * 5 * 1);
 
                             //kill
                             perGroupCount++;
@@ -358,7 +358,7 @@ namespace GridControl
                 while (pids.Count > 0)
                 {
                     //! 执行等待，然后查询更新pids列表.等待1分钟
-                    Console.WriteLine(string.Format("等待第{0}场{1}文件计算完成并关闭，pid进程查询更新等待中，等待时长15秒...", d + 1, curDatFullname) + DateTime.Now);
+                    Console.WriteLine(string.Format("等待第{0}场{1}文件计算完成并关闭，pid进程查询更新等待中，等待时长5秒...", d + 1, curDatFullname) + DateTime.Now);
                     System.Threading.Thread.Sleep(1000 * 5 * 1);
                     perWaitCount++;
                     Console.WriteLine(string.Format("最后进程组,已经等待次数{0}次", perWaitCount) + DateTime.Now);
@@ -628,7 +628,7 @@ namespace GridControl
                         {
                             //! 执行等待，然后查询更新pids列表.等待1分钟
                             Console.WriteLine(string.Format("共{0}个分组", processGroup) + DateTime.Now);
-                            Console.WriteLine(string.Format("等待第{0}场{1}文件的第{2}进程组计算完成并关闭，pid进程查询更新等待中，等待时长15秒...", d + 1, curDatFullname, g) + DateTime.Now);
+                            Console.WriteLine(string.Format("等待第{0}场{1}文件的第{2}进程组计算完成并关闭，pid进程查询更新等待中，等待时长5秒...", d + 1, curDatFullname, g) + DateTime.Now);
                             System.Threading.Thread.Sleep(1000 * 15 * 1);
 
                             //kill
