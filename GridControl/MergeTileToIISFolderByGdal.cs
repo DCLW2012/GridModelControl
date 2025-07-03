eef808a1fb50a0f9e9731390bbc8a0e5faab2f81
longyangzz <longyangzz@outlook.com>
2025-07-03 15:55:12 +0800

    fix: 优化升级降雨写出

--- GridControl/MergeTileToIISFolderByGdal.cs
+++ GridControl/MergeTileToIISFolderByGdal.cs
@@ -435,7 +435,7 @@ namespace GridControl
             Console.WriteLine("✅ GIF 创建完成！");
             return true;
         }
-        static public Rgba32 GenRGBColorByLegend(float value, float curMinValue, float curMaxValue)
+        static public Rgba32 GenRGBColorByLegend_discharge(float value, float curMinValue, float curMaxValue)
         {
             //0
             //0-10，10-20，20-50，50-100，100-200，200-500，500-1000，1000-2000，2000-5000，5000-10000，10000以上
@@ -488,6 +488,72 @@ namespace GridControl
             }
 
             return _cColors[index];
+        }
+
+        static public Rgba32 GenRGBColor(float value, float curMinValue, float curMaxValue)
+        {
+            //创建十个元素的list List<float>
+            List<float> _cValues = new List<float>(10);
+            for (int i = 0; i < 10; ++i)
+            {
+                _cValues.Add(0.0f);
+            }
+
+
+            //根据最小值，最大值，生成_cValues 区间。
+            for (int i = 0; i < 10; ++i)
+            {
+                if (i == 0)
+                {
+                    _cValues[0] = curMinValue;
+                }
+
+                if (i == 9)
+                {
+                    _cValues[9] = curMaxValue;
+                }
+
+                if (i > 0 && i < 9)
+                {
+                    _cValues[i] = (float)(curMinValue + (curMaxValue - curMinValue + 1.0e-6) / 10.0 * (float)i);
+                }
+            }
+
+            List<Rgba32> _cColors = new List<Rgba32>();
+            _cColors.Add(new Rgba32(255, 255, 255, 255));
+            _cColors.Add(new Rgba32(166, 242, 242, 255));
+            _cColors.Add(new Rgba32(61, 184, 63, 255));
+            _cColors.Add(new Rgba32(98, 184, 255, 255));
+            _cColors.Add(new Rgba32(0, 0, 253, 255));
+            _cColors.Add(new Rgba32(249, 1, 249, 255));
+            _cColors.Add(new Rgba32(127, 1, 64, 255));
+            _cColors.Add(new Rgba32(244, 167, 0, 255));
+            _cColors.Add(new Rgba32(235, 99, 0, 255));
+            _cColors.Add(new Rgba32(220, 0, 0, 255));
+            _cColors.Add(new Rgba32(147, 0, 0, 255));
+
+            if (value <= _cValues[0])
+            {
+                return _cColors[0];
+            }
+
+            if (value > _cValues[9])
+            {
+                return _cColors[10];
+            }
+
+            int index = 0;
+            for (int i = 0; i < _cValues.Count; ++i)
+            {
+                if (value > _cValues[i] && value <= _cValues[i + 1])
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return _cColors[index + 1];
+
         }
 
         static public Rgba32 GenRGBColorByLegend_water(float value, float curMinValue, float curMaxValue)
@@ -673,7 +739,11 @@ namespace GridControl
                             }
                             else
                             {
-                                Rgba32 color = GenRGBColorByLegend(value, curMinValue, curMaxValue);
+                                Rgba32 color = GenRGBColor(value, curMinValue, curMaxValue);
+                                if (legendType.ToLower().Equals("discharge"))
+                                {
+                                    color = GenRGBColorByLegend_discharge(value, curMinValue, curMaxValue);
+                                }
                                 if (legendType.ToLower().Equals("water_depth"))
                                 {
                                     color = GenRGBColorByLegend_water(value, curMinValue, curMaxValue);
